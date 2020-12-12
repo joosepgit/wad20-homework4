90f1cc5c14379b901d452f5fd37a5754de72af52
mAXeLLz <Max.Frolov007@gmail.com>
2020-12-12 23:00:19 +0200

    tests done

--- frontend/tests/unit/PostsTest.spec.js
+++ frontend/tests/unit/PostsTest.spec.js
@@ -2,6 +2,7 @@ import {mount, createLocalVue} from '@vue/test-utils'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
 import Posts from "../../src/components/Posts.vue";
+import moment from "moment";
 
 const localVue = createLocalVue();
 
@@ -100,7 +101,36 @@ describe('Posts', () => {
 
     const wrapper = mount(Posts, {router, store, localVue});
 
-    it('1 == 1', function () {
-        expect(true).toBe(true)
+    it('renders the correct number of posts', function () {
+        const posts = wrapper.findAll(".post")
+        expect(posts.length).toEqual(testData.length)
     });
+
+    it('media renders properties correctly', function () {
+        const posts = wrapper.findAll(".post")
+        //go throught every post separately
+        for (let i = 0; i < posts.length; i++) {
+            let selectedPost = posts.at(i)
+            let post = testData[i]
+            if (post.media) {
+                if (post.media.type === 'image'){
+                    expect(selectedPost.find('.post-image').find('img').exists()).toBe(true)
+                }
+                else if (post.media.type === 'video'){
+                    expect(selectedPost.find('.post-image').find('video').exists()).toBe(true)
+                }
+            } else {
+                expect(selectedPost.find('.post-image').exists()).toBe(false)
+            }
+        }
+    })
+
+    it('check if date format is correct', function () {
+        //check each post format
+        for (let i = 0; i < testData.length; i++) {
+            const post = testData[i]
+            const expected = moment(post.createTime).format('LLLL')
+            expect(wrapper.html()).toContain(expected)
+        }
+    })
 });
